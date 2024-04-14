efae68aa807ce1c86fe65d849d050988bfc46694
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-04-14 13:37:09 +0300

    Update <?php.php

--- <?php.php
+++ <?php.php
@@ -11,3 +11,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     }
 }
 ?>
+<?php
+$targetDir = "uploads/";
+$targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+
+if (file_exists($targetFile)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
