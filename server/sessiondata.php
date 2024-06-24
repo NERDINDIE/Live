62b0ff0ffc68ef4e33e97bf49b0e466f17ca3a08
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-06-24 14:35:23 +0300

    Create sessiondata.php

---
+++ server/sessiondata.php
@@ -0,0 +1,10 @@
+// Use secure session cookies
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'domain' => 'example.com',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
+session_start();
