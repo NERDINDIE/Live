71e609d7a95ed410ec0f6210dcfb47affaa16637
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-06-24 12:22:03 +0300

    Create sidenav.js

---
+++ misc/sidenav.js
@@ -0,0 +1,6 @@
+function toggleNav () {
+                var sideNav = document.querySelector ('.sidenav');
+                sidenav.classList.toggle ("open");
+            }
+            var toggleButton= document.querySelector ("#toggle-button");
+            toggleButton.addEventListener ("click", toggleNav);
