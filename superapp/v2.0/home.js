814a150ad0421757a651f18abeedf91a2586045c
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-07-11 12:20:12 +0300

    Create home.js

---
+++ superapp/v2.0/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Superapp</h1>
+      <p>This is a superapp with multiple integrated services.</p>
+    </div>
+  );
+};
+
+export default Home;
