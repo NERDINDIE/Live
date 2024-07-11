6d90887d8503202b82c971cbd3eb2fbbcc8bbce7
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-07-11 12:14:34 +0300

    Create signup.js

---
+++ superapp/signup.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post('https://yourapi.com/auth/signup', { email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Signup error', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleSignup}>Signup</button>
+    </div>
+  );
+};
+
+export default Signup;
