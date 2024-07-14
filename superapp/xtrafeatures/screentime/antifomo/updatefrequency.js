0fccd8ee042cc188471f6dda99bcc516eb41af32
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-07-14 18:30:11 +0300

    Create updatefrequency.js

---
+++ superapp/xtrafeatures/screentime/antifomo/updatefrequency.js
@@ -0,0 +1,5 @@
+document.getElementById('update-frequency').addEventListener('change', function() {
+    const frequency = this.value;
+    console.log(`User selected update frequency: ${frequency}`);
+    // Logic to update notification frequency
+});
