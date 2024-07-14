7c3adb440e3b0452103f07944b27eff8cd01a3a4
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-07-14 18:34:29 +0300

    Create snooze.js

---
+++ superapp/xtrafeatures/screentime/antifomo/snooze.js
@@ -0,0 +1,9 @@
+function snoozeNotifications() {
+    alert("Notifications snoozed for 1 hour.");
+    // Logic to snooze notifications
+}
+
+function enableDoNotDisturb() {
+    alert("Do Not Disturb enabled.");
+    // Logic to enable Do Not Disturb
+}
