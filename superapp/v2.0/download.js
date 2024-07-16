23f9aa4939e25b83c41abf2f9677d29d9199e2ad
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-07-16 17:10:11 +0300

    Create download.js

---
+++ superapp/v2.0/download.js
@@ -0,0 +1,21 @@
+document.getElementById('download-content').addEventListener('click', function() {
+    const content = {
+        tips: document.getElementById('tips-content').innerHTML,
+        articles: document.getElementById('articles-content').innerHTML,
+        fitness: document.getElementById('fitness-content').innerHTML,
+        events: {
+            movies: document.getElementById('movies').innerHTML,
+            plays: document.getElementById('plays').innerHTML,
+            concerts: document.getElementById('concerts').innerHTML
+        }
+    };
+
+    const blob = new Blob([JSON.stringify(content)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'health_subchannel_content.json';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
