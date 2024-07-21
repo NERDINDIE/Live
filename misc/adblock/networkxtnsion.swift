a93c1aa698e8e72d5db16eb3bb86350f3170f975
MNEWS <147854711+NERDINDIE@users.noreply.github.com>
2024-07-21 12:46:01 +0300

    Create networkxtnsion.swift

---
+++ misc/adblock/networkxtnsion.swift
@@ -0,0 +1,18 @@
+import NetworkExtension
+
+class AdBlockerVPN: NEPacketTunnelProvider {
+
+    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "10.0.0.1")
+        settings.ipv4Settings = NEIPv4Settings(addresses: ["10.0.0.2"], subnetMasks: ["255.255.255.0"])
+        settings.dnsSettings = NEDNSSettings(servers: ["8.8.8.8"])
+
+        setTunnelNetworkSettings(settings) { error in
+            completionHandler(error)
+        }
+    }
+
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
